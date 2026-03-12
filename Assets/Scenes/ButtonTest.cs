ffa54abf5f53ada8a018e88fbb185f637a8485d0
N4NH <nanhnotfound@gmail.com>
2026-03-12 09:53:09 +0700

    Initial commit

---
+++ Assets/Scenes/ButtonTest.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Start Game");
+    }
+
+    public void OpenOptions()
+    {
+        Debug.Log("Open Options");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit Game");
+    }
+}
